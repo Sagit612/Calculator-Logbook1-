8188a761c6290b73133f366e1cb74b02b97addb3
Vo Minh Quoc (Sagit) <84282841+Sagit612@users.noreply.github.com>
2023-10-02 22:32:10 +0700

    delete library

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,13 +3,8 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Scriptable;
 
 public class MainActivity extends AppCompatActivity {
 
